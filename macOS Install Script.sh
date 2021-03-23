ddd335e5fa9a7f9e01079429dbf727229c6c3974
MyYogurt <40012201+MyYogurt@users.noreply.github.com>
2021-03-23 14:08:19 -0400

    Add install script

---
+++ macOS Install Script.sh
@@ -0,0 +1,9 @@
+#1/bin/bash
+git clone https://github.com/MyYogurt/OpenDance.git
+cd OpenDance/
+git clone https://github.com/CMU-Perceptual-Computing-Lab/openpose
+cd openpose/
+git submodule update --init --recursive --remote
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew update
+bash /scripts/osx/install_deps.sh
