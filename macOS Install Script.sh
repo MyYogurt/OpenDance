eb061b41795175d2cc5a63aa6bb2552267c862d6
MyYogurt <40012201+MyYogurt@users.noreply.github.com>
2021-03-23 14:57:22 -0400

    Add download caffemodel

--- macOS Install Script.sh
+++ macOS Install Script.sh
@@ -4,6 +4,8 @@ cd OpenDance/
 git clone https://github.com/CMU-Perceptual-Computing-Lab/openpose
 cd openpose/
 git submodule update --init --recursive --remote
+cd ..
+curl http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel -o openpose/models/pose/mpi/pose_iter_160000.caffemodel
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
 bash /scripts/osx/install_deps.sh
